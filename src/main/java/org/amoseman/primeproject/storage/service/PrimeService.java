e9e5c7d52e8fd49a2d2f4cca775d46e90545cd88
a-moseman <drewmoseman@gmail.com>
2024-09-02 00:03:37 -0400

    Add javadoc

--- src/main/java/org/amoseman/primeproject/storage/service/PrimeService.java
+++ src/main/java/org/amoseman/primeproject/storage/service/PrimeService.java
@@ -3,9 +3,32 @@ package org.amoseman.primeproject.storage.service;
 import java.math.BigInteger;
 import java.util.List;
 
+/**
+ * Represents the interface of a prime service.
+ */
 public interface PrimeService {
+    /**
+     * Add a prime.
+     * @param prime the prime.
+     */
     void add(BigInteger prime);
+
+    /**
+     * Force the service to write any primes it may have in memory to the database.
+     */
     void write();
+
+    /**
+     * Get the last prime discovered.
+     * @return the last prime discovered.
+     */
     BigInteger last();
+
+    /**
+     * Get a list of primes in a range.
+     * @param offset the offset of the range.
+     * @param length the length of the range.
+     * @return the primes in the range.
+     */
     List<BigInteger> get(long offset, long length);
 }
